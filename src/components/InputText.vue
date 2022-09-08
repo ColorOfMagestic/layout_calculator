bb74a0302063d2e91ca6bec8bbcb2bebccf037ad
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-09-08 22:46:58 +0500

    компонент InputText

---
+++ src/components/InputText.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    class="calc_input calc_input_block calc_input--gen"
+    type="number"
+    min="0"
+    @input="$emit('input', $event.target.value)"
+    ref="input"
+  />
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  props: {
+    model: {
+      prop: 'value',
+      event: 'input'
+    },
+
+    price: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>

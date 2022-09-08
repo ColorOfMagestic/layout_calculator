917042a306de990f558b19625f5dff367d8394f2
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-09-08 22:47:25 +0500

    компонент LabelApp

---
+++ src/components/LabelApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <label class="calc_label">{{ title }}<slot></slot></label>
+</template>
+
+<script>
+export default {
+  name: 'LabelApp',
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
